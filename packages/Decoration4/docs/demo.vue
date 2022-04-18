0519559d50b59a42e9e40e1382ed143b2309d81f
苦尽甘来 <745385015@qq.com>
2022-04-18 16:55:18 +0800

    update 装饰4

--- packages/Decoration4/docs/demo.vue
+++ packages/Decoration4/docs/demo.vue
@@ -1,5 +1,11 @@
 <template>
   <div style="width:800px;height:200px;" flex="~ " justify-center items-center bg-dark>
-    <dv-decoration4 />
+    <dv-decoration4 :dur="2" style="width:5px;height:150px;"/>
+  </div>
+  <div text-2xl pt10>
+    dv-decoration4(reverse)
+  </div>
+  <div style="width:800px;height:200px;" flex="~ " justify-center items-center bg-dark>
+    <dv-decoration4 :reverse="true" style="width:250px;height:5px;"/>
   </div>
 </template>
