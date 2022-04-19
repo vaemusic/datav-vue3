4c0b96c5e9ae3c3527adbf53010910b22dfcbf0b
苦尽甘来 <745385015@qq.com>
2022-04-19 17:01:59 +0800

    完善数字翻拍器(未完成，更新数据，以及ts没做)

--- packages/DigitalFlop/docs/demo.vue
+++ packages/DigitalFlop/docs/demo.vue
@@ -1,5 +1,22 @@
 <template>
   <div style="width:800px;height:200px;" flex="~ " justify-center items-center bg-dark>
-    <dv-digital-flop />
+    <dv-digital-flop :config="config1"/>
+    <el-button @click="changeConfig">切换数据</el-button>
   </div>
 </template>
+<script setup>
+let config1 = {
+  number: [100],
+  content: '{nt}个'
+}
+
+const config2 = {
+  number: [999],
+  content: '{nt}个'
+}
+
+const changeConfig = () => {
+  config1 = { ...config2 }
+  console.log(config1)
+}
+</script>
