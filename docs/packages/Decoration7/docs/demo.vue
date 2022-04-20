3cebb5ad299c90d21ccd13c99be6d1bc213c8d25
苦尽甘来 <745385015@qq.com>
2022-04-20 14:11:24 +0800

    update docs

--- docs/packages/Decoration7/docs/demo.vue
+++ docs/packages/Decoration7/docs/demo.vue
@@ -1,5 +1,9 @@
 <template>
   <div style="width:800px;height:200px;" flex="~ " justify-center items-center bg-dark>
-    <dv-decoration7 />
+    <dv-decoration7 style="width:150px;height:30px;">
+      <div color-white font-300>
+        Decoration
+      </div>
+    </dv-decoration7>
   </div>
 </template>
