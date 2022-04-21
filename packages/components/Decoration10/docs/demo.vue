367953703e557aeb9bd9daed99d20635870c70fd
苦尽甘来 <745385015@qq.com>
2022-04-21 11:28:48 +0800

    完善装饰10

--- packages/components/Decoration10/docs/demo.vue
+++ packages/components/Decoration10/docs/demo.vue
@@ -1,5 +1,5 @@
 <template>
   <div style="width:800px;height:200px;" flex="~ " justify-center items-center bg-dark>
-    <dv-decoration10 />
+    <dv-decoration-10 style="width:90%;height:5px;" />
   </div>
 </template>
