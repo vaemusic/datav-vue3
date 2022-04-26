93a8dd5b0520dd542436a2c172d29ad891d8c466
苦尽甘来 <745385015@qq.com>
2022-04-26 15:08:10 +0800

    完善边框11

--- packages/docs/src/pages/BorderBox11/docs/demo.vue
+++ packages/docs/src/pages/BorderBox11/docs/demo.vue
@@ -1,5 +1,9 @@
 <template>
-  <div style="width:800px;height:200px;" flex="~ " justify-center items-center bg-dark>
-    <dv-border-box11 />
+  <div style="width:50rem;height:18rem;" p3 flex="~ " justify-center items-center bg-dark>
+    <dv-border-box-11 title="dv-border-box-11" :title-width="400">
+      <div w50rem h18rem color-white flex justify-center items-center>
+        dv-border-box-11
+      </div>
+    </dv-border-box-11>
   </div>
 </template>
