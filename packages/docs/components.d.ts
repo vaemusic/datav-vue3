1cff6118996ee068541fac3bc62c0c0a771b6018
苦尽甘来 <745385015@qq.com>
2022-04-27 16:33:41 +0800

    更新文档

--- packages/docs/components.d.ts
+++ packages/docs/components.d.ts
@@ -5,7 +5,6 @@
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     DvMenu: typeof import('./src/components/DvMenu.vue')['default']
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
