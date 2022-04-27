7c81084fa958eaa78324f3a0161d1f5e2d875d35
苦尽甘来 <745385015@qq.com>
2022-04-27 16:31:25 +0800

    修改文档中部分样式，和部分错误内容

--- packages/docs/src/pages/Decoration9/docs/demo.vue
+++ packages/docs/src/pages/Decoration9/docs/demo.vue
@@ -1,13 +1,13 @@
 <template>
   <div style="width:800px;height:200px;" flex="~ " justify-center items-center bg-dark>
-    <dv-decoration-9 style="width:150px;height:150px;" >
+    <dv-decoration-9 style="width:150px;height:150px;">
       <div color-green font-600 class="content" bg="~ dark/0">
         66%
-      </div> 
+      </div>
     </dv-decoration-9>
   </div>
 </template>
-<style>
+<style scoped>
   .content{
     font-size: 28px;
     text-shadow: 0 0 3px #7acaec;
