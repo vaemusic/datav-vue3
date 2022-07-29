a22025124319c2e0d680e2ba2f5f8b3a52363453
苦尽甘来 <745385015@qq.com>
2022-07-29 13:59:03 +0800

    chore: 删除多余文件

--- build/base.config.ts
+++ build/base.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-// import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
   resolve: {
@@ -28,9 +27,6 @@ export default defineConfig({
       ],
       dts: true,
     }),
-    // https://github.com/hannoeru/vite-plugin-pages
-    // Pages(),
-
   ],
 
   // https://github.com/vitest-dev/vitest
