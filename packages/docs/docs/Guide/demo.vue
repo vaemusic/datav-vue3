665d8f829ecca7a66f8272c9536e85cb9844fe23
苦尽甘来 <745385015@qq.com>
2022-09-18 01:26:13 +0800

    docs: 更新文档中边框注意事项，边框的类型定义

--- packages/docs/docs/Guide/demo.vue
+++ packages/docs/docs/Guide/demo.vue
@@ -1,11 +1,11 @@
 <template>
   <div flex="~ col" bg-dark>
     <div demo-bg :style="`width: ${width}rem`">
-      <dv-border-box-11 ref="dvBorder11" title="dv-border-box-11" :title-width="400">
+      <dv-border-box11 ref="dvBorder11" title="dv-border-box-11" :title-width="400">
         <div dv-bg>
           dv-border-box-11
         </div>
-      </dv-border-box-11>
+      </dv-border-box11>
     </div>
     <div self-center py5>
       <button @click="addWith" btn>增加边框组件父容器宽度</button>
@@ -13,14 +13,15 @@
   </div>
 </template>
 
-<script setup>
-const dvBorder11 = ref(null)
+<script setup lang="ts">
+import { BorderBox11 as DvBorderBox11 } from '@kjgl77/datav-vue3'
+const dvBorder11 = ref<InstanceType<typeof DvBorderBox11> | null>(null)
 
 const width = ref(40)
 
 // 每点击一次，边框组件父容器宽度加2rem
 const addWith = () => {
   width.value += 2
-  dvBorder11.value.initWH()
+  dvBorder11.value?.initWH()
 }
 </script>
