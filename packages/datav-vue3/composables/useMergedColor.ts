9d88d81471b87cc3907429cc93a222ef388146ab
vaemusic <745385015@qq.com>
2023-10-07 15:19:52 +0800

    chore: 修改 useMergedColor 注释

--- packages/datav-vue3/composables/useMergedColor.ts
+++ packages/datav-vue3/composables/useMergedColor.ts
@@ -1,7 +1,10 @@
 import type { Ref } from 'vue'
 
 /**
- * @description https://github.com/DataV-Team/DataV-Vue3/blob/main/src/hooks/useMergedColor.ts
+ * 如果 colors 没有传入颜色，则返回默认颜色，反之则返回传入的颜色
+ * @param defaultColors 默认颜色
+ * @param colors props 传入颜色
+ * @returns 最终颜色
  */
 export function useMergedColor(defaultColors: string[], colors: Ref<string[]>) {
   return computed(() => colors.value.length === 0 ? defaultColors : colors.value)
